adb2a5e82914b1b6a96be57a45dd5be0e74d21a8
Nepein Andrey <nepein96@gmail.com>
2020-06-20 14:40:11 +0300

    credentials header

--- src/index.js
+++ src/index.js
@@ -20,39 +20,39 @@ const app = express();
 const server = http.createServer(app);
 const RedisStore = redisStore(session);
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST
+  host: process.env.REDIS_HOST,
 });
 
 mongoose.connect(`mongodb://${process.env.MONGO_HOST}:27017/mail`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://епочта.рф:3000', 'http://епочта.рф'] }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://епочта.рф:3000', 'http://епочта.рф'],
+    credentials: true,
+  }),
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-    session({
-        store: new RedisStore({ client: redisClient }),
-        secret: 'keyboard cat',
-        resave: false,
-    })
-)
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'keyboard cat',
+    resave: false,
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 usePassportConfig(passport);
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
 app.use('/api/v1', router);
 
 const EMAIL_SERVER_HOST = process.env.NODE_ENV === 'production' ? 'backend' : 'localhost';
 
 const httpServer = server.listen(3000, EMAIL_SERVER_HOST, function () {
-    console.log('We are live on ', httpServer.address());
+  console.log('We are live on ', httpServer.address());
 });
