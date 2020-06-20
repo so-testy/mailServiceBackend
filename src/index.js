11b4c65a501e1fc8f6c43d7f97443693158660aa
Ladanov Nikolay <ladanov_n@mail.ru>
2020-06-20 20:01:12 +0300

    fixed inbox

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ mongoose.connect(`mongodb://${process.env.MONGO_HOST}:27017/mail`, {
 
 app.use(
   cors({
-    origin: ['http://localhost:3001', 'http://епочта.рф:3001', 'http://епочта.рф'],
+    origin: ['http://localhost:3001', 'http://епочта.рф:3001', 'http://епочта.рф', 'http://xn--80aj1adq3a.xn--p1ai'],
     credentials: true,
   }),
 );
